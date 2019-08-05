060d4dcf38711098f2a8f881543628b92c1ab96c
bechirmlika <45511907+bechirmlika@users.noreply.github.com>
2019-08-05 19:01:09 +0200

    Add files via upload

--- main/java/com/example/iptvsmarterspro/Reclamation.java
+++ main/java/com/example/iptvsmarterspro/Reclamation.java
@@ -2,6 +2,7 @@ package com.example.iptvsmarterspro;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -42,24 +43,30 @@ public class Reclamation extends AppCompatActivity {
         envoyer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendMail();
-            }
+                sendEmail(); }
 
 
-            private void sendMail() {
-                String recipientList = Destinataire.getText().toString();
-                String[] recipients = recipientList.split(",");
+            private void sendEmail() {
+               String recipientList = Destinataire.getText().toString();
+               String[] recipients = recipientList.split(",");
 
                 String subject = Objet.getText().toString();
                 String message = Reclamation.getText().toString();
+               // String name = Nom.getText().toString();
+                //String pre = Prenom.getText().toString();
 
                 Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+                //Intent intent = new Intent(Intent.ACTION_SENDTO);
+               intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+               // intent.setData(Uri.parse("mailto:" + "bechirmlika@gmail.com"));
                 intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-                intent.putExtra(Intent.EXTRA_TEXT, message);
-
+                intent.putExtra(Intent.EXTRA_TEXT,message);
+                try {
                 intent.setType("message/rfc822");
                 startActivity(Intent.createChooser(intent, "Choose an email client"));
+                 } catch (android.content.ActivityNotFoundException ex) {
+                           Toast.makeText(Reclamation.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+               }
             }
         });
     }
